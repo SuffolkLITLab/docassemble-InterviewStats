3933d16b6a48ef94da27de89a5dd88887a1c4e2c
System Administrator <qsteenhuis@gmail.com>
2020-09-05 20:59:26 -0400

    Correct the interview path

--- setup.py
+++ setup.py
@@ -43,9 +43,9 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.InterviewStats',
-      version='0.0.1',
+      version='0.0.2',
       description=('A docassemble extension.'),
-      long_description='# docassemble.InterviewStats\n\nA docassemble extension.\n\n## Author\n\nSystem Administrator, admin@admin.com\n\n',
+      long_description='# docassemble.InterviewStats\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='System Administrator',
       author_email='admin@admin.com',
