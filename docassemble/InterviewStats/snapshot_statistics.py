e637e46b3a00c07b7e6531599bd25c8979304485
System Administrator <qsteenhuis@gmail.com>
2020-11-15 15:11:03 -0500

    Start to generalize the use

--- docassemble/InterviewStats/snapshot_statistics.py
+++ docassemble/InterviewStats/snapshot_statistics.py
@@ -2,12 +2,10 @@ from docassemble.base.util import variables_snapshot_connection, user_info
 
 __all__ = ['get_stats']
 
-def get_stats(filename, variable_name):
+def get_stats(filename):
   conn = variables_snapshot_connection()
   cur = conn.cursor()
-  query = "select data->>'" # This is how we get SQL injection
-  query += variable_name
-  query += "' from jsonstorage where filename='" + filename + "'"
+  query = "select data from jsonstorage where filename='" + filename + "'"
   cur.execute(query)
   records = list()
   for record in cur.fetchall():
