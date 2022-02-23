d6b36593be6dba5f88375629bf7129a12bc2ba2d
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2022-02-22 23:21:20 -0500

    Got things going much faster!
    Still need to make Excel downloads work at big scales (timing out at 247k), but
    pretty good so far.

--- docassemble/InterviewStats/generate_test_data.py
+++ docassemble/InterviewStats/generate_test_data.py
@@ -1,14 +1,19 @@
-from docassemble.webapp.jsonstore import read_answer_json, write_answer_json
+from docassemble.webapp.jsonstore import read_answer_json, write_answer_json, JsonStorage, JsonDb
 from docassemble.base.generate_key import random_alphanumeric
 from docassemble.base.functions import get_current_info
 from docassemble.base.util import variables_snapshot_connection
 
-def write_random_user(data, filename=None, tags=None):
+__all__ = ['write_random_user', 'delete_data_from_interview']
+
+def write_random_user(data_iterator, filename=None, tags=None):
   if not filename:
     filename = get_current_info().get('yaml_filename', None)
-  random_uid = random_alphanumeric(32)
-  write_answer_json(random_uid, filename, data, tags, persistent=False)
-
+  for data in data_iterator:
+    random_uid = random_alphanumeric(32)
+    new_entry = JsonStorage(filename=filename, key=random_uid, data=data, tags=tags, persistent=False)
+    JsonDb.add(new_entry)
+  JsonDb.commit()
+  
 def delete_data_from_interview(filename=None):
   if not filename:
     filename = get_current_info().get('yaml_filename', None)
