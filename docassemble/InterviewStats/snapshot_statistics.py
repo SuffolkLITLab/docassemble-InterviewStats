ef6e825fb1a45e4f21d2bb63d1c2be13c4bc93c4
Quinten Steenhuis <qsteenhuis@gmail.com>
2022-07-05 10:18:57 -0400

    Draw the list of interviews from the `jsonstorage` table, and use interview_menu() only if filename present

--- docassemble/InterviewStats/snapshot_statistics.py
+++ docassemble/InterviewStats/snapshot_statistics.py
@@ -1,15 +1,32 @@
-from docassemble.base.util import variables_snapshot_connection
+from docassemble.base.util import variables_snapshot_connection, interview_menu
+from typing import List
 
-__all__ = ['get_filenames', 'get_summary_stats', 'get_stats', 'get_columns', 'get_column_values']
+__all__ = ['get_filenames', 'get_summary_stats', 'get_stats', 'get_columns', 'get_column_values', 'get_combined_filename_list', "get_overall_stats"]
 
 def get_filenames():
     conn = variables_snapshot_connection()
     with conn.cursor() as cur:
-        query = "select DISTINCT filename from jsonstorage"
+        query = "select DISTINCT(filename) as filename from jsonstorage WHERE tags is null or tags != 'metadata'"
         cur.execute(query)
         results = [record for record in cur]
     conn.close()
     return results
+
+
+def get_combined_filename_list():
+    json_filenames = get_filenames()
+    interview_filenames = interview_menu()
+    combined_interviews = []
+    for json_interview in json_filenames:
+        found_match = False
+        for interview in interview_filenames:
+            if interview["filename"] == json_interview[0]:
+                combined_interviews.append({interview["filename"]: interview.get("title", interview["filename"]) })
+                found_match = True
+                continue
+        if not found_match:
+            combined_interviews.append({json_interview[0]: json_interview[0]})
+    return combined_interviews
 
 def get_summary_stats(filename: str):
     conn = variables_snapshot_connection()
@@ -19,6 +36,17 @@ def get_summary_stats(filename: str):
         val = cur.fetchone()
     conn.close()
     return val
+
+
+def get_overall_stats():
+    conn = variables_snapshot_connection()
+    with conn.cursor() as cur:
+        query = "select COUNT(modtime), MIN(modtime), MAX(modtime) from jsonstorage where tags is null OR tags != 'metadata'"
+        cur.execute(query)
+        val = cur.fetchone()
+    conn.close()
+    return val
+  
       
 def get_stats(filename: str, column:str=None):
     conn = variables_snapshot_connection()
